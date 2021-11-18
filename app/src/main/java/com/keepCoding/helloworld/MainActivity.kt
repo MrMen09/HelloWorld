ab13b57e5fb29143748bda4943997547cdb6c1f8
MrLMen09 <lemendozat@correo.udistrital.edu.co>
2021-11-18 17:34:04 -0500

    Base proyecto en clase

---
+++ app/src/main/java/com/keepCoding/helloworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.keepCoding.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
